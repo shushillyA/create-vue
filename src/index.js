ac7fe236679671e22d7111a2d9c0cdb49995b393
shaoyile <sylyxxu@gmail.com>
2018-09-13 00:36:20 +0800

    feat: update

--- src/index.js
+++ src/index.js
@@ -4,17 +4,23 @@ document.addEventListener('DOMContentLoaded', function() {
   let opt = {
     el: '#app', 
     data: {
-      name: '检索中',
-      age: 30,
-      baba: 'syl'
+      name: 'loading',
+      age: 'loading',
+      todo1: 'loading',
+      todo2: 'loading'
     }
   }
+
+  window.data = opt.data
 
   // 实例化vue
   let vm = new Vue(opt)
 
   // 调试
   setTimeout(function() {
-    opt.data.name = 'syl'
+    opt.data.name='syl'
+    opt.data.age='25'
+    opt.data.todo1 = '肥家'
+    opt.data.todo2 = '打ps4'
   }, 2000)
 })
